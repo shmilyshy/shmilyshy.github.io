df79d317cbdcfe8033c33d8e5543a1335b3f3d85
codershy <211767848@qq.com>
2023-01-05 15:53:19 +0800

    Site updated: 2023-01-05 15:53:18

--- js/cursor.js
+++ js/cursor.js
@@ -1,3 +1,30 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 3000
+                });
+            }
+        })
+    }, 300);
+})
+
 var CURSOR;
 
 Math.lerp = (a, b, n) => (1 - n) * a + n * b;
@@ -81,3 +108,17 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+new Vue({
+    data: function () {
+        this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+        });
+    }
+})
