9d222ca7a12ab9f34a95f1d5deaa5ae4b7ab17b0
codershy <211767848@qq.com>
2023-01-06 18:00:45 +0800

    Site updated: 2023-01-06 18:00:44

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "zh",
+    "borderRadius": "10",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "city": "CN101260409",
+    "key": "0717cbe446fa4744adb4f0e2037dcd60"
+  }
+}
