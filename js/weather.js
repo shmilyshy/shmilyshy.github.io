384f68aa27d1c4f70b0968ebc49725647e5318b1
codershy <211767848@qq.com>
2023-01-11 21:30:02 +0800

    Site updated: 2023-01-11 21:30:02

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,25 @@
-WIDGET = {
-  "CONFIG": {
-    "modules": "01234",
-    "background": "5",
-    "tmpColor": "FFFFFF",
-    "tmpSize": "16",
-    "cityColor": "FFFFFF",
-    "citySize": "16",
-    "aqiColor": "FFFFFF",
-    "aqiSize": "16",
-    "weatherIconSize": "24",
-    "alertIconSize": "18",
-    "padding": "10px 10px 10px 10px",
-    "shadow": "0",
-    "language": "auto",
-    "borderRadius": "10",
-    "fixed": "true",
-    "vertical": "top",
-    "horizontal": "left",
-    "city": "CN101260409",
-    "left": "10",
-    "top": "50",
-    "key": "cd30317680a347748d9e67ea15e91873"
-  }
-}
+// WIDGET = {
+//   "CONFIG": {
+//     "modules": "01234",
+//     "background": "5",
+//     "tmpColor": "FFFFFF",
+//     "tmpSize": "16",
+//     "cityColor": "FFFFFF",
+//     "citySize": "16",
+//     "aqiColor": "FFFFFF",
+//     "aqiSize": "16",
+//     "weatherIconSize": "24",
+//     "alertIconSize": "18",
+//     "padding": "10px 10px 10px 10px",
+//     "shadow": "0",
+//     "language": "auto",
+//     "borderRadius": "10",
+//     "fixed": "true",
+//     "vertical": "top",
+//     "horizontal": "left",
+//     "city": "CN101260409",
+//     "left": "10",
+//     "top": "50",
+//     "key": "cd30317680a347748d9e67ea15e91873"
+//   }
+// }
